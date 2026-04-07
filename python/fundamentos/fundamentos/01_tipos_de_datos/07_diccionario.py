fe27535c885809c4cff8ed4d18370d0508622ba0
isiss641225 <isidoravalenzuela@liceovvh.cl>
2026-04-07 09:25:09 -0400

    e

---
+++ python/fundamentos/fundamentos/01_tipos_de_datos/07_diccionario.py
@@ -0,0 +1,48 @@
+#Diccionarios en python
+estudiante = {"nombre": "Gonzalo", "curso": "Python"} #Notación Literal
+#Imprimir el nombre de estudiante
+print(estudiante["nombre"]) #Imprime: Gonzalo
+
+estudiante["nombre"] = "Isidora"
+print(estudiante["nombre"]) #Imprime: Vicente
+
+
+#Diccionario de países
+paises = {} #Diccionario vacío
+paises["MEX"] = "México" #Agregando valores
+paises["COL"] = "Colombia"
+paises["CHL"] = "Chile" 
+paises["PER"] = "Perú"
+paises["ARG"] = "Argentina"
+
+
+#Condición para buscar elemento e insertar si no existe
+if "CRI" in paises: #Preguntamos si existe la clave en el diccionario
+   print("¿Deseas reemplazar el valor?")
+else: #No existe esa clave
+   paises["CRI"] = "Costa Rica"
+
+#Eliminar valores del diccionario
+valor_removido = paises.pop("MEX") #Elimina el elemento y devuelve su valor
+print(f"Valor removido: {valor_removido}")
+del paises["COL"] #Elimina el elemento
+print(paises) #Imprime: {'CHL': 'Chile'}
+
+
+#Diccionario Pintor multilínea
+pintor = {
+    "nombre": "Frida Kahlo", 
+    "pais": "México", 
+    "fecha_nacimiento": "6 de julio de 1907"
+    }
+
+
+#Diccionarios anidados
+escuela = {
+   "nombre": "Coding Dojo LATAM",
+   "profesores": [
+       {"nombre": "Alfredo", "apellido": "Salazar", "cursos": ["Python", "Java"]},
+       {"nombre": "Valeria", "apellido": "Romero", "cursos": ["Fundamentos", "Java"]},
+       {"nombre": "Marcelo", "apellido": "Argotti", "cursos":["MERN", "Python"]}
+   ]
+}
